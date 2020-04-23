62bc980190c59f95a9ee0d0d0bb292d607306f8f
auto-store <37550823+auto-store@users.noreply.github.com>
2020-04-23 15:24:44 +0100

    only aws in vars

--- variables.tf
+++ variables.tf
@@ -16,31 +16,31 @@ variable "hosted-zone" {
   description = "The name of the dns zone on Route 53 that will be used as the master zone "
 }
 
-# AWS
+// # AWS
 
-variable "create_aws_dns_zone" {
-  description = "Set to true if you want to deploy the AWS delegated zone."
-  type        = bool
-  default     = "false"
-}
+// variable "create_aws_dns_zone" {
+//   description = "Set to true if you want to deploy the AWS delegated zone."
+//   type        = bool
+//   default     = "false"
+// }
 
-variable "aws_region" {
-  description = "The region to create resources."
-  default     = "eu-west-2"
-}
+// variable "aws_region" {
+//   description = "The region to create resources."
+//   default     = "eu-west-2"
+// }
 
-# Azure
+// # Azure
 
-variable "create_azure_dns_zone" {
-  description = "Set to true if you want to deploy the Azure delegated zone."
-  type        = bool
-  default     = "false"
-}
+// variable "create_azure_dns_zone" {
+//   description = "Set to true if you want to deploy the Azure delegated zone."
+//   type        = bool
+//   default     = "false"
+// }
 
-variable "azure_location" {
-  description = "The azure location to deploy the DNS service"
-  default     = "West Europe"
-}
+// variable "azure_location" {
+//   description = "The azure location to deploy the DNS service"
+//   default     = "West Europe"
+// }
 
 # GCP
 
